d5ae975a16d7080cccfe54de1d65ecba4cf40949
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-16 00:07:48 +0300

    added PostsController

---
+++ SocialNetwork.Api/Controllers/PostsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Api.Data.Services.Implementation;
+
+namespace SocialNetwork.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly PostService _postService;
+
+        public PostsController(PostService postService)
+        {
+            _postService = postService;
+        }
+    }
+}
