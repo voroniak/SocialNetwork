6ee83d985b38a9658cf2b33c70bb3c9844f4671d
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-15 23:44:33 +0300

    added Like and Post entity

---
+++ SocialNetwork.Api/Data/Repository/Entities/Like.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Api.Data.Repository.Entities
+{
+    public class Like: Document
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public ObjectId UserId { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public ObjectId LikedEntityId { get; set; }
+
+    }
+}
---
+++ SocialNetwork.Api/Data/Repository/Entities/Post.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Api.Data.Repository.Entities
+{
+    public class Post : Document
+    {
+        public string Text { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.String)]
+        public ObjectId UserId { get; set; }
+
+
+    }
+}
