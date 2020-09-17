c693cb129e183bca3c58c2f79179fc5a461a60ec
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-17 20:02:15 +0300

    added AutoMapper

--- SocialNetwork.Api/Startup.cs
+++ SocialNetwork.Api/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using AspNetCore.Identity.MongoDbCore.Extensions;
 using AspNetCore.Identity.MongoDbCore.Infrastructure;
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -71,6 +72,7 @@ namespace SocialNetwork.Api
             services.AddScoped<JwtService>();
             services.AddScoped<PostService>();
             services.AddControllers().AddNewtonsoftJson(options => options.UseMemberCasing());
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
