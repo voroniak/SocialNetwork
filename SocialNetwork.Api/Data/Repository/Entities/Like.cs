51c8b2734e44a757ccbbd5df4c6e23def324a66c
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-17 19:52:09 +0300

    fixed entities

--- SocialNetwork.Api/Data/Repository/Entities/Like.cs
+++ SocialNetwork.Api/Data/Repository/Entities/Like.cs
@@ -1,20 +1,10 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SocialNetwork.Api.Data.Repository.Entities
+﻿namespace SocialNetwork.Api.Data.Repository.Entities
 {
-    public class Like: Document
+    [BsonCollection("Likes")]
+    public class Like : Document
     {
-        [BsonId]
-        [BsonRepresentation(BsonType.String)]
-        public ObjectId UserId { get; set; }
-        [BsonId]
-        [BsonRepresentation(BsonType.String)]
-        public ObjectId LikedEntityId { get; set; }
+        public string UserId { get; set; }
+        public string LikedEntityId { get; set; }
 
     }
 }
--- SocialNetwork.Api/Data/Repository/Entities/Post.cs
+++ SocialNetwork.Api/Data/Repository/Entities/Post.cs
@@ -6,10 +6,7 @@ namespace SocialNetwork.Api.Data.Repository.Entities
     public class Post : Document
     {
         public string Text { get; set; }
-        
+
         public string UserId { get; set; }
-
-        public IEnumerable<string> Likes { get; set; }
-
     }
 }
