b970f17ef1b858508791b86048373e90f07013c7
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-18 20:24:49 +0300

    added mapping to user

---
+++ SocialNetwork.Api/Data/Mapping/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SocialNetwork.Api.Data.DTOs;
+using SocialNetwork.Api.Data.Repository.Entities;
+
+namespace SocialNetwork.Api.Data.Mapping
+{
+    public class UserProfile: Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<ApplicationUser, UserDto>().ReverseMap();
+        }
+    }
+}
