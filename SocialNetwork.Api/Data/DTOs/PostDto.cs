ad7e633fb5684a528faed02ed5df3ec81e771d58
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-18 20:33:24 +0300

    added mapping for post

---
+++ SocialNetwork.Api/Data/DTOs/PostDto.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Api.Data.DTOs
+{
+    public class PostDto
+    {
+        ObjectId Id { get; set; }
+
+        DateTime CreatedAt { get; }
+        public string Text { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
---
+++ SocialNetwork.Api/Data/Mapping/PostPofile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using SocialNetwork.Api.Data.DTOs;
+using SocialNetwork.Api.Data.Repository.Entities;
+namespace SocialNetwork.Api.Data.Mapping
+{
+    public class PostPofile: Profile
+    {
+        public PostPofile()
+        {
+            CreateMap<PostDto, Post>().ReverseMap();
+        }
+    }
+}
