b6b96a682f256e6fdae2f99b36817dccb24d3d85
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-18 23:06:34 +0300

    added LikesController and add method

---
+++ SocialNetwork.Api/Controllers/LikesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Api.Data.DTOs;
+using SocialNetwork.Api.Data.Services.Implementation;
+
+namespace SocialNetwork.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikesController : ControllerBase
+    {
+        private readonly LikeService _likeService;
+
+        public LikesController(LikeService likeService)
+        {
+            _likeService = likeService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(LikePostDto likePostDto)
+        {
+            await _likeService.Add(likePostDto);
+
+            return Created("Add", likePostDto);
+        }
+    }
+}
