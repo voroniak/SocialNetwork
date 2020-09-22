dcc3047133ede243f91698c1d0d03f02364b7fe4
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-22 17:05:07 +0300

    added Comment entity

---
+++ SocialNetwork.Api/Data/Repository/Entities/Comment.cs
@@ -0,0 +1,10 @@
+﻿
+namespace SocialNetwork.Api.Data.Repository.Entities
+{
+    public class Comment: Document
+    {
+        public string UserId { get; set; }
+        public string Text { get; set; }
+        public string CommentedEntity { get; set; }
+    }
+}
