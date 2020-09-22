bf99565a01bb36e47e9c66789c01910404568435
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-22 17:16:58 +0300

    added CommentService

---
+++ SocialNetwork.Api/Data/Services/Implementation/CommentService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SocialNetwork.Api.Data.Repository.Entities;
+using SocialNetwork.Api.Data.Repository.Repo;
+
+namespace SocialNetwork.Api.Data.Services.Implementation
+{
+    public class CommentService
+    {
+        private readonly IMongoRepository<Comment> _mongoRepository;
+        private readonly IMapper _mapper;
+
+        public CommentService(IMongoRepository<Comment> mongoRepository, IMapper mapper)
+        {
+            _mongoRepository = mongoRepository;
+            _mapper = mapper;
+        }
+    }
+}
