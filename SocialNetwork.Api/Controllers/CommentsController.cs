a0a498af0255c4f582aa3e291dbe320f85e35ad7
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-23 18:54:53 +0300

    added CommentsController

---
+++ SocialNetwork.Api/Controllers/CommentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        public CommentsController()
+        {
+                
+        }
+    }
+}
