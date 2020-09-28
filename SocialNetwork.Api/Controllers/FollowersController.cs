0011abd288c2360479aba6b0650224864ab14447
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-28 22:11:39 +0300

    added methods to FollowersController

--- SocialNetwork.Api/Controllers/FollowersController.cs
+++ SocialNetwork.Api/Controllers/FollowersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SocialNetwork.Api.Data.DTOs;
 using SocialNetwork.Api.Data.Services.Implementation;
 
 namespace SocialNetwork.Api.Controllers
@@ -17,6 +18,34 @@ namespace SocialNetwork.Api.Controllers
         public FollowersController(FollowerService followerService)
         {
             _followerService = followerService;
+        }
+
+        [HttpPost("Follow")]
+        public async Task<IActionResult> FollowUser(FollowDto followDto)
+        {
+            await _followerService.FollowUserAsync(followDto.FollowerUserId, followDto.FollowingUserId);
+            
+            return NoContent();
+        }
+
+        [HttpPost("Unfollow")]
+        public async Task<IActionResult> UnfollowUser(FollowDto followDto)
+        {
+            await _followerService.UnfollowUserAsync(followDto.FollowerUserId, followDto.FollowingUserId);
+
+            return NoContent();
+        }
+
+        [HttpGet("Followers")]
+        public async Task<IActionResult> GetAllUserFollowers(string userId)
+        {
+           return Ok(await _followerService.GetAllUserFollowers(userId));
+        }
+
+        [HttpGet("Following")]
+        public async Task<IActionResult> GetAllUserFollowing(string userId)
+        {
+          return Ok(  await _followerService.GetAllUserFollowing(userId));
         }
     }
 }
---
+++ SocialNetwork.Api/Data/DTOs/FollowDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialNetwork.Api.Data.DTOs
+{
+    public class FollowDto
+    {
+        public string FollowerUserId { get; set; }
+        public string FollowingUserId { get; set; }
+    }
+}
