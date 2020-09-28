9aefff9b4ab9a98d2f94cf8375f84710f4b8a63f
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-28 12:21:36 +0300

    added FollowerService

---
+++ SocialNetwork.Api/Data/Services/Implementation/FollowerService.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SocialNetwork.Api.Data.Repository.Entities;
+using SocialNetwork.Api.Data.Repository.Repo;
+
+namespace SocialNetwork.Api.Data.Services.Implementation
+{
+    public class FollowerService
+    {
+        private readonly IMongoRepository<User> _mongoRepository;
+        private readonly IMapper _mapper;
+
+        public FollowerService(IMongoRepository<User> mongoRepository, IMapper mapper)
+        {
+            _mongoRepository = mongoRepository;
+            _mapper = mapper;
+        }
+    }
+}
