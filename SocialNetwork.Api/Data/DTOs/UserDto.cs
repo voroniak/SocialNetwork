6606ac28772287342e3b27bf1173c118cde3d282
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-09-28 21:20:26 +0300

    changed user dto

--- SocialNetwork.Api/Data/DTOs/UserDto.cs
+++ SocialNetwork.Api/Data/DTOs/UserDto.cs
@@ -1,9 +1,14 @@
-﻿namespace SocialNetwork.Api.Data.DTOs
+﻿using System.Collections.Generic;
+
+namespace SocialNetwork.Api.Data.DTOs
 {
     public class UserDto
     {
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public IEnumerable<string> Interests { get; set; }
+        public IEnumerable<string> Followers { get; set; }
+        public string AplplicationUserId { get; set; }
     }
 }
