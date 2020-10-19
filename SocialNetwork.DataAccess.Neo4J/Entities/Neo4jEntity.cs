7971455e23299d967af3281e0cfa168c8a27a41f
Олег Вороняк <oleg.voronyakk@gmail.com>
2020-10-19 15:21:23 +0300

    added  Neo4jRelationship and Neo4jEntity

---
+++ SocialNetwork.DataAccess.Neo4J/Entities/Neo4jEntity.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using SocialNetwork.DataAccess.Neo4J.Interfaces;
+using System;
+using System.Xml.Serialization;
+
+namespace SocialNetwork.DataAccess.Neo4J.Entities
+{
+    public class Neo4jEntity : IEntity
+    {
+        protected Neo4jEntity()
+        {
+            LastUpdated = DateTime.UtcNow;
+        }
+
+        [JsonIgnore]
+        [XmlIgnore]
+        public string Label { get; protected set; }
+
+        [JsonIgnore]
+        [XmlIgnore]
+        public DateTimeOffset LastUpdated { get; set; }
+    }
+}
---
+++ SocialNetwork.DataAccess.Neo4J/Entities/Neo4jRelationship.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace SocialNetwork.DataAccess.Neo4J.Entities
+{
+    public class Neo4jRelationship
+    {
+        [JsonIgnore]
+        public string Name { get; set; }
+    }
+}
